de08ddfca2c0081c3ab1491b20162cd8d9426340
doug2991 <da_go_92@hotmail.com>
2016-07-24 10:59:18 -0600

    Commit message

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <!DOCTYPE>
+<div class="container">
+
+
 <HEAD>
-<TITLE>Hedoku prueba!</TITLE>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+	<TITLE>Douglas Guillen IST Test</TITLE>
+
+
+
+	<!-- Agrega referencia a JavaScript-->
+	<script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+
+	<script src="js/funciones.js" type="text/javascript"></script>
+
+	<!--Agrega referencia al Bootstrap-->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+
 </HEAD>
 <BODY>
-	<div>
-		<h1>Pagina de Prueba</h1>
+
+<!--Este el titulo de la pagina principal-->
+	<div class="page-header">
+		<h1>Laureate International Support Team
+			<small>Test IS Tech Full Time</small></h1>
 	</div>
+
+<!--Esta es la barra de navegacion del menu principal -->
+<div class="row">
+	<nav class="navbar navbar-default">
+		<div class="btn-group btn-group-lg" role="group">
+			<button type="button" class="btn btn-default" id="btnLoad_Template">Load Template</button>
+			<button type="button" class="btn btn-default" id="btnGenerate">Generate</button>
+		</div>
+	</nav>
+</div>
+
+<div class="container" id="principal_container">
+
+
+</div>
+
 </BODY>
+</div>
